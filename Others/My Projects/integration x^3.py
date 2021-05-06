46aacfb942ea5aac04e7cb00835aa381df258fd6
PeithonKing <amukherjeeniser@gmail.com>
2021-05-06 15:40:50 +0530

    update

--- Others/My Projects/integration x^3.py
+++ Others/My Projects/integration x^3.py
@@ -1,9 +1,17 @@
-a = 1
-b = 2
-m = 10**5
+import math
+
+def f(x):
+	return (1-math.cos(x))
+	
+lower_limit = 0
+upper_limit = 2*math.pi
+# lower_limit = int(input("Input lower limit: "))
+# upper_limit = int(input("Input upper limit: "))
+m = 10**4
+
 ys = []
-for value in range(a*m,b*m):
-    y = ((value/m)**3)/m
+for value in range(int(lower_limit*m), int(upper_limit*m)):
+    y = (f(value/m))/m
     ys.append(y)
-int = sum(ys)
-print("The integration of x^3 from " + str(a) + " to " + str(b) + " is, " + str(int))
+inte = sum(ys)
+print("The integration of f(x) from " + str(lower_limit) + " to " + str(upper_limit) + " is, " + '{:.10f}'.format(inte))
--- Others/My Projects/spam.py
+++ Others/My Projects/spam.py
@@ -1,9 +1,5 @@
 import pyautogui, time
 time.sleep(3)
 for i in range(100):
-	# pyautogui.typewrite("*")
-	pyautogui.typewrite(str(i+1) + ") Hey handsome wanna have threesome?")
-	pyautogui.press("enter")
-	pyautogui.typewrite("Though I know your next step is blocking, wont you love a quick ass-fucking!!")
-	pyautogui.press("enter")
+	pyautogui.typewrite("Kaustav stop spamming!")
 	pyautogui.press("enter")
